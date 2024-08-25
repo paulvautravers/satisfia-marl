604297861d3d9eeb81c18c8c29e2f1354783a470
Paul Vautravers <paulvautravers23@hotmail.com>
2024-08-25 14:37:08 +0100

    14-adding-tests: Adding MC tests, and moved features into utils for testing

--- tests/test_monte_carlo.py
+++ tests/test_monte_carlo.py
@@ -9,6 +9,8 @@ class MonteCarloTests(unittest.TestCase):
     N_AGENTS = 10  # per type
 
     def setUp(self):
+
+        Agent.reset()
         self.game = defaultGame
         self.strategy_set = {TesterAgent: {TesterAgent: [0.5, 0.5], TesterAgent2: [0.5, 0.5]},  # 50-50 defect, cooperate
                              TesterAgent2: {TesterAgent: [0.5, 0.5], TesterAgent2: [0.5, 0.5]}}
@@ -23,4 +25,39 @@ class MonteCarloTests(unittest.TestCase):
         self.assertEqual(self.mc.generations, self.generations)
         self.assertTrue(np.array_equal(self.mc.agent_list, self.agents))
         self.assertIsInstance(self.mc.reward_dict, defaultdict)
-        self.assertEqual(self.mc.agent_counts, {TesterAgent: [self.N_AGENTS], TesterAgent2: [self.N_AGENTS]})
+
+    def test_get_current_agent_coutnts(self):
+        counts = self.mc.get_current_agent_counts()
+        for i in range(self.N_AGENTS):
+            new_agent = TesterAgent()
+            self.mc.agent_list = np.append(self.mc.agent_list, new_agent)
+            counts = self.mc.get_current_agent_counts()
+            self.assertEqual(counts, {TesterAgent: self.N_AGENTS+(i+1), TesterAgent2: self.N_AGENTS})
+
+    def test_store_agent_counts(self):
+
+        self.mc.store_agent_counts()
+        self.assertEqual(self.mc.agent_counts, {TesterAgent: [self.N_AGENTS, self.N_AGENTS],
+                         TesterAgent2: [self.N_AGENTS, self.N_AGENTS]})
+
+    def test_set_agent_by_id(self):  # this seems odd - is this desired behaviour?
+        new_agent = TesterAgent()
+        new_agent.id = 0
+        self.mc.set_agent_by_id(0, new_agent)
+        self.assertEqual(self.mc.agent_list[0], new_agent)
+
+    def test_set_agent_with_invalid_id(self):
+        new_agent = TesterAgent()
+        new_agent.id = 999
+        with self.assertRaises(ValueError):
+            self.mc.set_agent_by_id(999, new_agent)
+
+    def test_invalid_agent_by_id_set(self):
+        new_agent = TesterAgent()
+        new_agent.id = 37
+        with self.assertRaises(AssertionError):
+            self.mc.set_agent_by_id(5, new_agent)
+
+
+
+
--- tests/utils.py
+++ tests/utils.py
@@ -19,5 +19,6 @@ class TesterAgent2(Agent):
 
     def get_action(self, opponent: Agent) -> int: return 1
 
+
 prisoners_dilemma = np.array([[(1, 1), (3, 0)], [(0, 3), (2, 2)]], dtype=(int, int))
 defaultGame = Game(prisoners_dilemma)
